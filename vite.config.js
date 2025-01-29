5dab5b3997d9e05e7dfe61a19316d459fe3589e9
YohanF1245 <yohan.ferdin@hotmail.fr>
2025-01-29 20:22:47 +0100

    chore: configuration initiale du projet et docker

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ export default defineConfig({
     plugins: [sveltekit()],
     server: {
         host: '0.0.0.0',
-        port: 3000
+        port: 3000,
+        fs: {
+            allow: ['..']
+        },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     }
 }); 
