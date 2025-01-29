40ff215ce64fc284596ebeaa0ae85e7ea277eca0
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-01-29 21:50:51 +0100

    chore: configuration initiale du projet

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,8 @@ export default defineConfig({
         },
         proxy: {
             '/api': {
-                target: 'http://localhost:3001',
+                target: process.env.BACKEND_URL || 'http://pigeon-nutrition-back-backend-1:3000',
                 changeOrigin: true,
-                secure: false,
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
